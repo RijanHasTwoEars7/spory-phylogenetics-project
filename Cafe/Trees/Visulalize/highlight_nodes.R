e71a561fa6d9073cfcf0ee45b291ca5007c3b0d0
Rijan Dhakal <rijan_dhakal@outlook.com>
2023-10-10 10:15:50 -0500

    This changes merges the functionality of the `cafe_tree.R` script and the `highlight_nodes.R` scripts into one.
    Previously `cafe_tree.R` and `highlight_nodes.R` did basically the same thing but except that `cafe_tree.R` used the labels in the newikc and `highlight_nodes.R` used the labels as received from parsing.
    
    Now the annotation table is optional, and `--label` flag can be used to determine which labels to use. This is boolean based `--label` is false by default and will use the parsed labels.
    
    If the `--label` flag is used and given `TRUE` it will use the newick labels.

--- Cafe/Trees/Visulalize/highlight_nodes.R
+++ Cafe/Trees/Visulalize/highlight_nodes.R
@@ -7,11 +7,13 @@
 
 library(optparse)
 library(ggtree)
+library(data.table)
 
 option_list = list(
   make_option("--table", type="character", default=NULL, help="The table that contains the data for the nodes to be highlighted. Supply tsv or change delimiter when df is being read in. The two columns should be 'Node' for nodes to be labelled and 'Name' for their names."),
   make_option("--tree", type="character", default=NULL, help="The name of the file that contains the tree in newick format."),
-  make_option("--plot", type="character", default=NULL, help="The full name for the output file.")
+  make_option("--plot", type="character", default=NULL, help="The full name for the output file."),
+  make_option("--label", type="character", default=FALSE, help="Should the label provided in the newick files be used instead of the label generated from parsing?")
 )
 
 opt_parser = OptionParser(option_list=option_list)
@@ -19,18 +21,34 @@ opt_parser = OptionParser(option_list=option_list)
 args = commandArgs(trailingOnly = TRUE)
 opt = parse_args(opt_parser, args=args)
 
-if (!file.exists(opt$table)) {
-    stop(opt$table, "could not be found. Please check that this file exists or if the correct locaiton was supplied.")
-}
-
 if (!file.exists(opt$tree)) {
-    stop(opt$tree, "could not be found. Please check that this file exists or if the correct locaiton was supplied.")
+  stop(opt$tree, "could not be found. Please check that this file exists or if the correct locaiton was supplied.")
 }
 
 the_tree <- read.tree(opt$tree)
 
-df <- read.table(opt$table, header=TRUE, sep=",")
 
-ggtree(the_tree, branch.length = "none") + geom_hilight(data=df, aes(node=Node, fill=Name)) + geom_tiplab(align=TRUE, linesize=.5) + geom_text2(aes(label = ifelse(!isTip, node, "")), size = 3,parse=TRUE, hjust=-0.1) + xlim(0,25)
+if(is.null(opt$table)){
 
-ggsave(opt$plot, width = 60, height = 60, units = "cm", limitsize = FALSE) 
+  if(opt$label) { # if false use the labels as provided by parsing
+    ggtree(the_tree, branch.length = "none") + geom_tiplab(align=TRUE, linesize=.5) + geom_text2(aes(label = ifelse(!isTip, node, "")), size = 3,parse=TRUE, hjust=-0.1) + xlim(0,25)
+  } else{ # if true then use the labels in the newick
+    ggtree(the_tree, branch.length = "none") + geom_text2(aes(label = label), size = 3, hjust=-0.1) + xlim(0,25)
+  }
+} else{
+
+  if (!file.exists(opt$table)) {
+  stop(opt$table, "could not be found. Please check that this file exists or if the correct locaiton was supplied.")
+  }
+
+  df <- fread(opt$table, header=TRUE)
+
+  if(opt$label) { # if false use the labels as provided by parsing
+    ggtree(the_tree, branch.length = "none") + geom_hilight(data=df, aes(node=Node, fill=Name)) + geom_tiplab(align=TRUE, linesize=.5) + geom_text2(aes(label = ifelse(!isTip, node, "")), size = 3,parse=TRUE, hjust=-0.1) + xlim(0,25)
+  } else{ # if true then use the labels in the newick
+    ggtree(the_tree, branch.length = "none") + geom_hilight(data=df, aes(node=Node, fill=Name)) + geom_text2(aes(label = label), size = 3, hjust=-0.1) + xlim(0,25)
+  } 
+
+}
+
+ggsave(opt$plot, width = 60, height = 60, units = "cm", limitsize = FALSE)
