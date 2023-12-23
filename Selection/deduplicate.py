286887c2e7638a0f59bf378cbc434bfd3837b0b1
Rijan Dhakal <rijan_dhakal@outlook.com>
2023-12-23 15:42:47 -0600

    New version of the , one that can account for files and directories

--- Selection/deduplicate.py
+++ Selection/deduplicate.py
@@ -15,19 +15,37 @@ def remove_duplicates(input_file, output_file):
 
 def main():
     parser = argparse.ArgumentParser(description='Remove duplicate sequences from a multi-fasta file.')
-    parser.add_argument('-i', '--input', required=True, help='Input fasta file')
-    parser.add_argument('-o', '--output', help='Output fasta file')
+    parser.add_argument('-i', '--input', required=True, help='Input fasta file or directory')
+    parser.add_argument('-o', '--output', help='Output fasta file or directory')
 
     args = parser.parse_args()
 
-    input_file = args.input
-    if args.output:
-        output_file = args.output
-    else:
-        base_name = os.path.splitext(input_file)[0]
-        output_file = f"{base_name}.no_duplicates.fa"
+    input_path = args.input
+    output_path = args.output
 
-    remove_duplicates(input_file, output_file)
+    if os.path.isdir(input_path):
+        # If input is a directory, loop over all files in the directory
+        for filename in os.listdir(input_path):
+            if filename.endswith(".fasta"):
+                input_file = os.path.join(input_path, filename)
+                if output_path:
+                    # If output directory is provided, use it
+                    output_file = os.path.join(output_path, f"{os.path.splitext(filename)[0]}.no_duplicates.fa")
+                else:
+                    # If no output directory is provided, create a new one based on the input directory name
+                    new_dir = f"{input_path}_no_duplicates"
+                    os.makedirs(new_dir, exist_ok=True)
+                    output_file = os.path.join(new_dir, f"{os.path.splitext(filename)[0]}.no_duplicates.fa")
+                remove_duplicates(input_file, output_file)
+    else:
+        # If input is a file, process it as before
+        if output_path:
+            output_file = output_path
+        else:
+            base_name = os.path.splitext(input_path)[0]
+            output_file = f"{base_name}.no_duplicates.fa"
+        remove_duplicates(input_path, output_file)
 
 if __name__ == "__main__":
     main()
+
